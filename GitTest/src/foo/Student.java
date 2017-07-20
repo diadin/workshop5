485b7ba8e8d42fd5fe6a9dc896028f53d6aa3e6c
diana.moldoveanu <diana.moldoveanu@mmshodev.asf.madm.net>
2017-07-20 15:53:50 +0300

    My first commit in git

---
+++ GitTest/src/foo/Student.java
@@ -0,0 +1,18 @@
+package foo;
+
+public class Student {
+ public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+private int age;
+ private String name;
+}
---
+++ GitTest/src/foo/StudentConsumer.java
@@ -0,0 +1,13 @@
+package foo;
+
+public class StudentConsumer {
+
+	public static void main(String[] args) {
+		Student s1= new Student();
+		//s1.setName("Ion");
+	//	s1.setAge(24);
+     //   System.out.println("hello "+ s1.getName());
+        System.out.println("hello");
+	}
+
+}
