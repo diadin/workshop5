7dd34f3b0c0590a81cba2fab71cb66f99caccb38
diana.moldoveanu <diana.moldoveanu@mmshodev.asf.madm.net>
2017-07-20 15:55:14 +0300

    Al doilea commit

--- GitTest/src/foo/StudentConsumer.java
+++ GitTest/src/foo/StudentConsumer.java
@@ -4,10 +4,10 @@ public class StudentConsumer {
 
 	public static void main(String[] args) {
 		Student s1= new Student();
-		//s1.setName("Ion");
-	//	s1.setAge(24);
-     //   System.out.println("hello "+ s1.getName());
-        System.out.println("hello");
+		s1.setName("Ion");
+		s1.setAge(24);
+        System.out.println("hello "+ s1.getName());
+      //  System.out.println("hello");
 	}
 
 }
